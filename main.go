3cad7c5e824df7a9a76643461321a91da2e4be7e
Liang <1595500083121005@mil>
2021-06-22 11:05:12 -0400

    added apis for inclinometer

--- main.go
+++ main.go
@@ -89,8 +89,8 @@ func main() {
 
 	e := echo.New()
 	e.Use(middleware.CORS, middleware.GZIP)
-	public := e.Group(cfg.RoutePrefix) // TODO: /instrumentation/v1/
-
+	//public := e.Group(cfg.RoutePrefix) // TODO: /instrumentation/v1/
+	public := e.Group("/instrumentation") // TODO: /instrumentation/v1/
 	// Media Routes
 	public.GET("/projects/:project_slug/images/*", handlers.GetMedia(awsCfg, &cfg.AWSS3Bucket, "/midas", &cfg.RoutePrefix))
 
@@ -139,11 +139,11 @@ func main() {
 	CACOnly.Use(middleware.EDIPIMiddleware, middleware.CACOnlyMiddleware)
 
 	// Profile and Tokens
-	CACOnly.POST("/profiles", handlers.CreateProfile(db))
-	CACOnly.GET("/my_profile", handlers.GetMyProfile(db))
-	CACOnly.GET("/my_projects", handlers.ListMyProjects(db))
-	CACOnly.POST("/my_tokens", handlers.CreateToken(db))
-	CACOnly.DELETE("/my_tokens/:token_id", handlers.DeleteToken(db))
+	public.POST("/profiles", handlers.CreateProfile(db))
+	public.GET("/my_profile", handlers.GetMyProfile(db))
+	public.GET("/my_projects", handlers.ListMyProjects(db))
+	public.POST("/my_tokens", handlers.CreateToken(db))
+	public.DELETE("/my_tokens/:token_id", handlers.DeleteToken(db))
 
 	// Authenticated with Appkey only (routes only to be used by other components of the app)
 	// Routes do not have /project/:project_id context and are typically authorized
@@ -167,21 +167,21 @@ func main() {
 	// AlertConfigs
 	public.GET("/projects/:project_id/instruments/:instrument_id/alert_configs", handlers.ListInstrumentAlertConfigs(db))
 	public.GET("/projects/:project_id/instruments/:instrument_id/alert_configs/:alert_config_id", handlers.GetAlertConfig(db))
-	private.POST("/projects/:project_id/instruments/:instrument_id/alert_configs", handlers.CreateInstrumentAlertConfigs(db))
-	private.PUT("/projects/:project_id/instruments/:instrument_id/alert_configs/:alert_config_id", handlers.UpdateInstrumentAlertConfig(db))
-	private.DELETE("/projects/:project_id/instruments/:instrument_id/alert_configs/:alert_config_id", handlers.DeleteInstrumentAlertConfig(db))
+	public.POST("/projects/:project_id/instruments/:instrument_id/alert_configs", handlers.CreateInstrumentAlertConfigs(db))
+	public.PUT("/projects/:project_id/instruments/:instrument_id/alert_configs/:alert_config_id", handlers.UpdateInstrumentAlertConfig(db))
+	public.DELETE("/projects/:project_id/instruments/:instrument_id/alert_configs/:alert_config_id", handlers.DeleteInstrumentAlertConfig(db))
 
 	// Alerts
 	public.GET("/projects/:project_id/instruments/:instrument_id/alerts", handlers.ListAlertsForInstrument(db))
 	private.GET("/my_alerts", handlers.ListMyAlerts(db)) // Private because token required to determine user (i.e. who is "me")
-	private.POST("/my_alerts/:alert_id/read", handlers.DoAlertRead(db))
-	private.POST("/my_alerts/:alert_id/unread", handlers.DoAlertUnread(db))
+	public.POST("/my_alerts/:alert_id/read", handlers.DoAlertRead(db))
+	public.POST("/my_alerts/:alert_id/unread", handlers.DoAlertUnread(db))
 
 	// AlertSubscriptions
-	private.GET("/my_alert_subscriptions", handlers.ListMyAlertSubscriptions(db)) // Private because token required to determine user (i.e. who is "me")
-	private.POST("/projects/:project_id/instruments/:instrument_id/alert_configs/:alert_config_id/subscribe", handlers.SubscribeProfileToAlerts(db))
-	private.POST("/projects/:project_id/instruments/:instrument_id/alert_configs/:alert_config_id/unsubscribe", handlers.UnsubscribeProfileToAlerts(db))
-	private.PUT("/alert_subscriptions/:alert_subscription_id", handlers.UpdateMyAlertSubscription(db))
+	public.GET("/my_alert_subscriptions", handlers.ListMyAlertSubscriptions(db)) // Private because token required to determine user (i.e. who is "me")
+	public.POST("/projects/:project_id/instruments/:instrument_id/alert_configs/:alert_config_id/subscribe", handlers.SubscribeProfileToAlerts(db))
+	public.POST("/projects/:project_id/instruments/:instrument_id/alert_configs/:alert_config_id/unsubscribe", handlers.UnsubscribeProfileToAlerts(db))
+	public.PUT("/alert_subscriptions/:alert_subscription_id", handlers.UpdateMyAlertSubscription(db))
 
 	// Email Autocomplete
 	public.GET("/email_autocomplete", handlers.ListEmailAutocomplete(db))
@@ -193,66 +193,66 @@ func main() {
 	public.GET("/projects/:project_id/instruments", handlers.ListProjectInstruments(db))
 	public.GET("/projects/:project_id/instruments/names", handlers.ListProjectInstrumentNames(db))
 	public.GET("/projects/:project_id/instrument_groups", handlers.ListProjectInstrumentGroups(db))
-	private.POST("/projects", handlers.CreateProjectBulk(db), middleware.IsApplicationAdmin)
-	private.PUT("/projects/:project_id", handlers.UpdateProject(db))
-	private.DELETE("/projects/:project_id", handlers.DeleteFlagProject(db))
+	public.POST("/projects", handlers.CreateProjectBulk(db), middleware.IsApplicationAdmin)
+	public.PUT("/projects/:project_id", handlers.UpdateProject(db))
+	public.DELETE("/projects/:project_id", handlers.DeleteFlagProject(db))
 
 	// Project Membership
 	// // list project memberships
-	private.GET("/projects/:project_id/members", handlers.ListProjectMembers(db))
+	public.GET("/projects/:project_id/members", handlers.ListProjectMembers(db))
 	// add role to a user
-	private.POST("/projects/:project_id/members/:profile_id/roles/:role_id", handlers.AddProjectMemberRole(db))
+	public.POST("/projects/:project_id/members/:profile_id/roles/:role_id", handlers.AddProjectMemberRole(db))
 	// remove role from a user
-	private.DELETE("/projects/:project_id/members/:profile_id/roles/:role_id", handlers.RemoveProjectMemberRole(db))
+	public.DELETE("/projects/:project_id/members/:profile_id/roles/:role_id", handlers.RemoveProjectMemberRole(db))
 
 	// Project Timeseries
-	private.POST("/projects/:project_id/timeseries/:timeseries_id", handlers.CreateProjectTimeseries(db))
-	private.DELETE("/projects/:project_id/timeseries/:timeseries_id", handlers.DeleteProjectTimeseries(db))
+	public.POST("/projects/:project_id/timeseries/:timeseries_id", handlers.CreateProjectTimeseries(db))
+	public.DELETE("/projects/:project_id/timeseries/:timeseries_id", handlers.DeleteProjectTimeseries(db))
 
 	// Instruments
 	public.GET("/instruments", handlers.ListInstruments(db))
 	public.GET("/instruments/count", handlers.GetInstrumentCount(db))
 	public.GET("/instruments/:instrument_id", handlers.GetInstrument(db))
-	private.POST("/projects/:project_id/instruments", handlers.CreateInstruments(db))
+	public.POST("/projects/:project_id/instruments", handlers.CreateInstruments(db))
 	// TODO: Remove endpoint POST /instruments (no project context)
-	private.POST("/instruments", handlers.CreateInstruments(db))
-	private.PUT("/projects/:project_id/instruments/:instrument_id", handlers.UpdateInstrument(db))
-	private.PUT("/projects/:project_id/instruments/:instrument_id/geometry", handlers.UpdateInstrumentGeometry(db))
-	private.DELETE("/projects/:project_id/instruments/:instrument_id", handlers.DeleteFlagInstrument(db))
+	public.POST("/instruments", handlers.CreateInstruments(db))
+	public.PUT("/projects/:project_id/instruments/:instrument_id", handlers.UpdateInstrument(db))
+	public.PUT("/projects/:project_id/instruments/:instrument_id/geometry", handlers.UpdateInstrumentGeometry(db))
+	public.DELETE("/projects/:project_id/instruments/:instrument_id", handlers.DeleteFlagInstrument(db))
 
 	// Instrument Groups
 	public.GET("/instrument_groups", handlers.ListInstrumentGroups(db))
 	public.GET("/instrument_groups/:instrument_group_id", handlers.GetInstrumentGroup(db))
 	public.GET("/instrument_groups/:instrument_group_id/instruments", handlers.ListInstrumentGroupInstruments(db))
 	public.GET("/instrument_groups/:instrument_group_id/timeseries", handlers.ListInstrumentGroupTimeseries(db))
-	private.POST("/instrument_groups", handlers.CreateInstrumentGroup(db))
-	private.PUT("/instrument_groups/:instrument_group_id", handlers.UpdateInstrumentGroup(db))
-	private.DELETE("/instrument_groups/:instrument_group_id", handlers.DeleteFlagInstrumentGroup(db))
+	public.POST("/instrument_groups", handlers.CreateInstrumentGroup(db))
+	public.PUT("/instrument_groups/:instrument_group_id", handlers.UpdateInstrumentGroup(db))
+	public.DELETE("/instrument_groups/:instrument_group_id", handlers.DeleteFlagInstrumentGroup(db))
 	// Add or Remove instrument from Instrument Group
-	private.POST("/instrument_groups/:instrument_group_id/instruments", handlers.CreateInstrumentGroupInstruments(db))
-	private.DELETE("/instrument_groups/:instrument_group_id/instruments/:instrument_id", handlers.DeleteInstrumentGroupInstruments(db))
+	public.POST("/instrument_groups/:instrument_group_id/instruments", handlers.CreateInstrumentGroupInstruments(db))
+	public.DELETE("/instrument_groups/:instrument_group_id/instruments/:instrument_id", handlers.DeleteInstrumentGroupInstruments(db))
 
 	// Instrument Constants (same as a timeseries in structure/payload)
 	public.GET("/projects/:project_id/instruments/:instrument_id/constants", handlers.ListInstrumentConstants(db))
-	private.POST("/projects/:project_id/instruments/:instrument_id/constants", handlers.CreateInstrumentConstants(db))
-	private.DELETE("/projects/:project_id/instruments/:instrument_id/constants/:timeseries_id", handlers.DeleteInstrumentConstant(db))
+	public.POST("/projects/:project_id/instruments/:instrument_id/constants", handlers.CreateInstrumentConstants(db))
+	public.DELETE("/projects/:project_id/instruments/:instrument_id/constants/:timeseries_id", handlers.DeleteInstrumentConstant(db))
 
 	// Instrument Notes(GET, PUT, DELETE work with or without instrument context in URL)
 	public.GET("/instruments/notes", handlers.ListInstrumentNotes(db))
 	public.GET("/instruments/notes/:note_id", handlers.GetInstrumentNote(db))
 	public.GET("/instruments/:instrument_id/notes", handlers.ListInstrumentInstrumentNotes(db))
 	public.GET("/instruments/:instrument_id/notes/:note_id", handlers.GetInstrumentNote(db))
-	private.POST("/instruments/notes", handlers.CreateInstrumentNote(db))
-	private.PUT("/instruments/notes/:note_id", handlers.UpdateInstrumentNote(db))
-	private.DELETE("/instruments/notes/:note_id", handlers.DeleteInstrumentNote(db))
-	private.PUT("/instruments/:instrument_id/notes/:note_id", handlers.UpdateInstrumentNote(db))
-	private.DELETE("/instruments/:instrument_id/notes/:note_id", handlers.DeleteInstrumentNote(db))
+	public.POST("/instruments/notes", handlers.CreateInstrumentNote(db))
+	public.PUT("/instruments/notes/:note_id", handlers.UpdateInstrumentNote(db))
+	public.DELETE("/instruments/notes/:note_id", handlers.DeleteInstrumentNote(db))
+	public.PUT("/instruments/:instrument_id/notes/:note_id", handlers.UpdateInstrumentNote(db))
+	public.DELETE("/instruments/:instrument_id/notes/:note_id", handlers.DeleteInstrumentNote(db))
 
 	// Instrument Status
 	public.GET("/instruments/:instrument_id/status", handlers.ListInstrumentStatus(db))
 	public.GET("/instruments/:instrument_id/status/:status_id", handlers.GetInstrumentStatus(db))
-	private.POST("/instruments/:instrument_id/status", handlers.CreateOrUpdateInstrumentStatus(db))
-	private.DELETE("/instruments/:instrument_id/status/:status_id", handlers.DeleteInstrumentStatus(db))
+	public.POST("/instruments/:instrument_id/status", handlers.CreateOrUpdateInstrumentStatus(db))
+	public.DELETE("/instruments/:instrument_id/status/:status_id", handlers.DeleteInstrumentStatus(db))
 
 	// Timeseries
 	public.GET("/projects/:project_id/timeseries", handlers.ListProjectTimeseries(db))
@@ -263,28 +263,28 @@ func main() {
 	public.GET("/timeseries/:timeseries_id/measurements", handlers.ListTimeseriesMeasurements(db))
 	public.GET("/instruments/:instrument_id/timeseries/:timeseries_id/measurements", handlers.ListTimeseriesMeasurements(db))
 	// TODO: Delete timeseries endpoints without project context in URL
-	private.POST("/timeseries", handlers.CreateTimeseries(db))
-	private.PUT("/timeseries/:timeseries_id", handlers.UpdateTimeseries(db))
-	private.DELETE("/timeseries/:timeseries_id", handlers.DeleteTimeseries(db))
-	private.POST("/projects/:project_id/timeseries_measurements", handlers.CreateOrUpdateProjectTimeseriesMeasurements(db))
+	public.POST("/timeseries", handlers.CreateTimeseries(db))
+	public.PUT("/timeseries/:timeseries_id", handlers.UpdateTimeseries(db))
+	public.DELETE("/timeseries/:timeseries_id", handlers.DeleteTimeseries(db))
+	public.POST("/projects/:project_id/timeseries_measurements", handlers.CreateOrUpdateProjectTimeseriesMeasurements(db))
 
 	// Collection Groups
 	public.GET("/projects/:project_id/collection_groups", handlers.ListCollectionGroups(db))
 	public.GET("/projects/:project_id/collection_groups/:collection_group_id", handlers.GetCollectionGroupDetails(db))
-	private.POST("/projects/:project_id/collection_groups", handlers.CreateCollectionGroup(db))
-	private.PUT("/projects/:project_id/collection_groups/:collection_group_id", handlers.UpdateCollectionGroup(db))
-	private.DELETE("/projects/:project_id/collection_groups/:collection_group_id", handlers.DeleteCollectionGroup(db))
+	public.POST("/projects/:project_id/collection_groups", handlers.CreateCollectionGroup(db))
+	public.PUT("/projects/:project_id/collection_groups/:collection_group_id", handlers.UpdateCollectionGroup(db))
+	public.DELETE("/projects/:project_id/collection_groups/:collection_group_id", handlers.DeleteCollectionGroup(db))
 	// // Collection Group; Add Timeseries to collection_group
-	private.POST("/projects/:project_id/collection_groups/:collection_group_id/timeseries/:timeseries_id", handlers.AddTimeseriesToCollectionGroup(db))
+	public.POST("/projects/:project_id/collection_groups/:collection_group_id/timeseries/:timeseries_id", handlers.AddTimeseriesToCollectionGroup(db))
 	// // Collection Group; Remove Timeseries from collection_group
-	private.DELETE("/projects/:project_id/collection_groups/:collection_group_id/timeseries/:timeseries_id", handlers.RemoveTimeseriesFromCollectionGroup(db))
+	public.DELETE("/projects/:project_id/collection_groups/:collection_group_id/timeseries/:timeseries_id", handlers.RemoveTimeseriesFromCollectionGroup(db))
 
 	// Plotting Configurations
 	public.GET("/projects/:project_id/plot_configurations", handlers.ListPlotConfigurations(db))
 	public.GET("/projects/:project_id/plot_configurations/:plot_configuration_id", handlers.GetPlotConfiguration(db))
-	private.POST("/projects/:project_id/plot_configurations", handlers.CreatePlotConfiguration(db))
-	private.PUT("/projects/:project_id/plot_configurations/:plot_configuration_id", handlers.UpdatePlotConfiguration(db))
-	private.DELETE("/projects/:project_id/plot_configurations/:plot_configuration_id", handlers.DeletePlotConfiguration(db))
+	public.POST("/projects/:project_id/plot_configurations", handlers.CreatePlotConfiguration(db))
+	public.PUT("/projects/:project_id/plot_configurations/:plot_configuration_id", handlers.UpdatePlotConfiguration(db))
+	public.DELETE("/projects/:project_id/plot_configurations/:plot_configuration_id", handlers.DeletePlotConfiguration(db))
 
 	// Misc
 	public.GET("/domains", handlers.GetDomains(db))
@@ -300,6 +300,6 @@ func main() {
 		log.Fatal(gateway.ListenAndServe("localhost:3030", e))
 	} else {
 		log.Print("starting server")
-		log.Fatal(http.ListenAndServe(":80", e))
+		log.Fatal(http.ListenAndServe(":8080", e))
 	}
 }
